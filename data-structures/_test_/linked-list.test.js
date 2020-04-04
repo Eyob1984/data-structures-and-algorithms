3b547722c71628e57322f72cb081d0e6eaaa2f6b
Eyob1984 <tamireyob@gmail.com>
2020-04-03 19:05:30 -0700

    append method working

--- data-structures/_test_/linked-list.test.js
+++ data-structures/_test_/linked-list.test.js
@@ -2,6 +2,15 @@
 
 const LinkedList = require('../linkedList/linked-list.js');
 
+/*
+Can successfully add a node to the end of the linked list
+Can successfully add multiple nodes to the end of a linked list
+Can successfully insert a node before a node located i the middle of a linked list
+Can successfully insert a node before the first node of a linked list
+Can successfully insert after a node in the middle of the linked list
+Can successfully insert a node after the last node of the linked list
+*/
+
 describe('passes all lab tests', () => {
     it('successfully instantiates an empty list', () => {
         expect(() => {
@@ -13,7 +22,6 @@ describe('passes all lab tests', () => {
         let list1 = new LinkedList();
         let val = list1.insert('A');
         let str = list1.toString();
-        console.log('checking str', str)
 
         expect(str).toBe('{' + val + '} -> null');
     });
@@ -33,6 +41,31 @@ describe('passes all lab tests', () => {
       list1.insert('C');
       expect(list1.head.data).toBe('C');
   });
+
+  it('successfully add a node to the end of the linked list', () => {
+    expect(() => {
+        let list1 = new LinkedList();
+        list1.append(1)
+        list1.append(2)
+        list1.append(3)
+        list1.append(5)
+        console.log('who are you', list1.head.data)
+        expect(list1.head.data).toBe(5)
+    })
+});
+  it('successfully add multiple nodes to the end of a linked list', () => {
+    expect(() => {
+        let list1 = new LinkedList();
+        list1.insertBefore(1)
+        list1.insertBefore(2)
+        list1.insertBefore(3)
+        list1.insertBefore(5)
+        expect(list1.head.data).toBe(3, 5)
+    })
+})
+
+
+
 });
 
 
--- data-structures/linkedList/linked-list.js
+++ data-structures/linkedList/linked-list.js
@@ -28,8 +28,47 @@ class LinkedList{
       node = node.next;
     }
     return false;
-
   }
+  append(data) {
+    let newNode = new Node(data);
+    let current;
+    if (this.head === null){
+      this.head = newNode;
+    }else{
+      current = this.head;
+      while (current.next){
+        current = current.next
+      }
+      current.next = newNode;
+    }
+  }
+  insertBefore(val, newData){
+    let node = this.head;
+    // console.log('the first head', node)
+    if(!node){
+      console.log('opps');
+    }
+      else if(node.data === val){
+          // console.log('nodedata', node.data, 'val', val)
+          node = new Node(newData, node)
+          // console.log('inside else if', node)
+          return;
+      }
+         while (node.next ) {
+           if(node.next.data === val){
+             let newVal = new Node(newData, node.next)
+             console.log('after inst', newVal)
+             node.next = newVal;
+             console.log('inside while', newVal)
+             return
+           }
+           node = node.next;
+
+         }
+    
+    // return 'Exception';
+  }
+
   toString(){
     let currNode =this.head;
     let string = '';
@@ -43,12 +82,15 @@ class LinkedList{
   }
 
 }
-// let list1 = new LinkedList()
-// list1.insert(100);
-// list1.insert(200);
-// list1.insert(300);
-// console.log(list1.includes(150));
-// list1.toString();
+let list1 = new LinkedList()
+list1.insert(100);
+list1.insert(200);
+list1.insert(300);
+list1.append(400)
+list1.insert(500)
+list1.insertBefore(500, 700)
+console.log(list1.includes(150));
+list1.toString();
 
 module.exports = LinkedList;
 
