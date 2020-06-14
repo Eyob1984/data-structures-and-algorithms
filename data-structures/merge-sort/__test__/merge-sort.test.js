7d8844dc593439f6cbc412238cac71c1d393d05a
Eyob1984 <tamireyob@gmail.com>
2020-06-14 16:52:08 -0700

    README updated

---
+++ data-structures/merge-sort/__test__/merge-sort.test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const mergeSort = require('../merge-sort.js');
+
+describe('Happy path', () => {
+  it('can successfully sort the array low to high', ()=>{
+    const num = [8, 2, 7, 3, 9, 1];
+    expect(mergeSort(num)).toStrictEqual([1, 2, 3, 7, 8, 9])
+  
+  })
+})
+
+describe('Edge casese', () => {
+  it('can successfully sort a negative number low to high', () => {
+    const num = [6, -5, 8, 1, -9, 7];
+    expect(mergeSort(num)).toEqual([-9, -5, 1, 6, 7, 8])
+  })
+})
